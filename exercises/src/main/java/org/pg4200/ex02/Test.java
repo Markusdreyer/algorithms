f368f8ef1b67054db25c664f330257d4c4f93750
Markus Dreyer <markus.a.dreyer@gmail.com>
2019-08-22 19:35:17 +0200

    ex02 part b progress

--- exercises/src/main/java/org/pg4200/ex02/Test.java
+++ exercises/src/main/java/org/pg4200/ex02/Test.java
@@ -2,25 +2,18 @@ package org.pg4200.ex02;
 
 public class Test {
     private static MyArrayListResizable list = new MyArrayListResizable();
+    private static MyRingArrayQueue queue = new MyRingArrayQueue(5);
 
     //EXERCISE
 
     public static void main(String[] args) {
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
-
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
-
-        list.add(0, 2);
-        list.add(0, 2);
-        list.add(0, 2);
+        //list.add(0, 2);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.dequeue();
 
     }
 }
