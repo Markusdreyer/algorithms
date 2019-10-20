c589d71b4a68d0676099157eb7a3cbd0d4b0d1d3
Markus Dreyer <markus.a.dreyer@gmail.com>
2019-10-20 18:09:27 +0200

    added MinInsertionSort algorithm

---
+++ exercises/src/main/java/org/pg4200/ExamAlgorithms/MinInsertionSort.java
@@ -0,0 +1,25 @@
+package org.pg4200.ExamAlgorithms;
+
+import org.pg4200.les03.sort.MySort;
+
+public class MinInsertionSort implements MySort {
+    @Override
+    public <T extends Comparable<T>> void sort(T[] array) {
+        if(array == null) {
+            return;
+        }
+
+        for(int i = 1; i < array.length; i++) {
+
+            for(int j = i - 1; j >= 0; j--) {
+                if(array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
--- exercises/src/main/java/org/pg4200/ExamAlgorithms/Visualizer.java
+++ exercises/src/main/java/org/pg4200/ExamAlgorithms/Visualizer.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 public class Visualizer  {
 
     public static void main(String[] args) {
-        MinLinkedList linkedList = new MinLinkedList();
+        MinInsertionSort insertionSort = new MinInsertionSort();
 
-        linkedList.add(0, "Hei");
+        Integer[] array = {7, 6, 3, 9, 5};
 
-        System.out.println(linkedList.get(0));
+        insertionSort.sort(array);
+
+        System.out.println(Arrays.deepToString(array));
     }
 }
